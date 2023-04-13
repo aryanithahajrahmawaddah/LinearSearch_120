19fa9862a2fa08b321402176539f54e9360eb709
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-04-13 14:50:38 +1200

    melakukan prosedur input

--- LinearSearch_120/LinearSearch_120.cpp
+++ LinearSearch_120/LinearSearch_120.cpp
@@ -4,3 +4,27 @@ using namespace std;
 int arr[20];		// Array to be searched
 int n;				// Number of elements in the array
 int i;				// Index of array element
+
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+	// Accept array elements
+	cout << "\n------------------\n";
+	cout << " Enter array elements ";
+	cout << "----------------------";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
