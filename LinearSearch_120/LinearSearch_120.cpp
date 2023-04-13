60dfdd2030650cc56e28971ce0d7b0c522606a4d
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-04-13 15:29:41 +1200

    langkah no 5 algoritma

--- LinearSearch_120/LinearSearch_120.cpp
+++ LinearSearch_120/LinearSearch_120.cpp
@@ -45,5 +45,12 @@ void LinearSearch()
 		for (i = 0; i < n; i++)									// Langkah no 2 & 3 & 4 Algoritma
 		{
 			ctr++;
-			
+			if (arr[i] == item)									// Langkah no 5 Algoritma
+			{
+				cout << "\n" << item << "found at position " << (i + 1) << endl;
+				break;
+			}
+		}
+
+
 		
